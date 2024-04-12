2a45395e716cc6bde1c3eeb1c3f7d88747221c93
Hellblazer <hellblazer@me.com>
2024-04-12 08:08:47 -0700

    Run Delphinious Oracle smoke test on bootstrapped group.
    increase timeout for CI

--- nut/src/test/java/com/hellblazer/nut/E2ETest.java
+++ nut/src/test/java/com/hellblazer/nut/E2ETest.java
@@ -130,7 +130,7 @@ public class E2ETest {
         System.out.println();
 
         var domains = sphinxes.subList(0, 4);
-        Utils.waitForCondition(30_000, 1_000, () -> failures.get() ? true : domains.stream().allMatch(s -> s.active()));
+        Utils.waitForCondition(120_000, 1_000, () -> failures.get() ? true : domains.stream().allMatch(s -> s.active()));
         assertTrue(domains.stream().allMatch(s -> s.active()),
                    "** Minimal quorum did not become active : " + (domains.stream()
                                                                           .filter(c -> !c.active())
@@ -162,7 +162,7 @@ public class E2ETest {
             System.out.println();
         });
 
-        Utils.waitForCondition(30_000, 1_000, () -> failures.get() ? true : sphinxes.stream().allMatch(Sphinx::active));
+        Utils.waitForCondition(120_000, 1_000, () -> failures.get() ? true : sphinxes.stream().allMatch(Sphinx::active));
         if (!sphinxes.stream().allMatch(Sphinx::active)) {
             System.out.println();
             fail("\n\nNodes did not fully activate: \n" + (sphinxes.stream()
